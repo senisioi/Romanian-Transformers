ac99989fdca5501830bdd486abded8429c382807
avramandrei <avram.andreimarius@gmail.com>
2020-05-28 20:34:57 +0300

    Refactored code for average v2.

--- evaluation/tools/results_average.py
+++ evaluation/tools/results_average.py
@@ -17,95 +17,71 @@ def print_task_metrics(task_name, dict_metrics, counter, sub_unit=False):
         ))
 
 
+def update_metrics(filename, lang_model_name, dict_metrics):
+    with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
+        dict_new_metrics = json.load(file)
+
+    for metric in dict_metrics:
+        dict_metrics[metric]["precision"] += dict_new_metrics[metric]["precision"]
+        dict_metrics[metric]["recall"] += dict_new_metrics[metric]["recall"]
+        dict_metrics[metric]["f1"] += dict_new_metrics[metric]["f1"]
+
+def init_metrics_dictionaries():
+    dict_metrics_ronec = {
+        "Entity Type": {"precision": 0, "recall": 0, "f1": 0},
+        "Partial": {"precision": 0, "recall": 0, "f1": 0},
+        "Strict": {"precision": 0, "recall": 0, "f1": 0},
+        "Exact": {"precision": 0, "recall": 0, "f1": 0}
+    }
+    dict_metrics_ronec_frozen = {
+        "Entity Type": {"precision": 0, "recall": 0, "f1": 0},
+        "Partial": {"precision": 0, "recall": 0, "f1": 0},
+        "Strict": {"precision": 0, "recall": 0, "f1": 0},
+        "Exact": {"precision": 0, "recall": 0, "f1": 0}
+    }
+    dict_metrics_rrt = {
+        "UPOS": {"precision": 0, "recall": 0, "f1": 0},
+        "XPOS": {"precision": 0, "recall": 0, "f1": 0}
+    }
+    dict_metrics_rrt_frozen = {
+        "UPOS": {"precision": 0, "recall": 0, "f1": 0},
+        "XPOS": {"precision": 0, "recall": 0, "f1": 0}
+    }
+    dict_metrics_rrt_udify = {
+        "UPOS": {"precision": 0, "recall": 0, "f1": 0},
+        "UFeats": {"precision": 0, "recall": 0, "f1": 0},
+        "Lemmas": {"precision": 0, "recall": 0, "f1": 0},
+        "LAS": {"precision": 0, "recall": 0, "f1": 0}
+    }
+
+    return dict_metrics_rrt_frozen, dict_metrics_rrt, \
+           dict_metrics_rrt_udify, \
+           dict_metrics_ronec_frozen, dict_metrics_ronec
+
+
 def main():
     for lang_model_name in os.listdir(args.results_path):
-        dict_metrics_ronec = {
-            "Entity Type": {"precision": 0, "recall": 0, "f1": 0},
-            "Partial": {"precision": 0, "recall": 0, "f1": 0},
-            "Strict": {"precision": 0, "recall": 0, "f1": 0},
-            "Exact": {"precision": 0, "recall": 0, "f1": 0}
-        }
-        dict_metrics_ronec_frozen = {
-            "Entity Type": {"precision": 0, "recall": 0, "f1": 0},
-            "Partial": {"precision": 0, "recall": 0, "f1": 0},
-            "Strict": {"precision": 0, "recall": 0, "f1": 0},
-            "Exact": {"precision": 0, "recall": 0, "f1": 0}
-        }
-        dict_metrics_rrt = {
-            "UPOS": {"precision": 0, "recall": 0, "f1": 0},
-            "XPOS": {"precision": 0, "recall": 0, "f1": 0}
-        }
-        dict_metrics_rrt_frozen = {
-            "UPOS": {"precision": 0, "recall": 0, "f1": 0},
-            "XPOS": {"precision": 0, "recall": 0, "f1": 0}
-        }
-        dict_metrics_rrt_udify = {
-            "UPOS": {"precision": 0, "recall": 0, "f1": 0},
-            "UFeats": {"precision": 0, "recall": 0, "f1": 0},
-            "Lemmas": {"precision": 0, "recall": 0, "f1": 0},
-            "LAS": {"precision": 0, "recall": 0, "f1": 0}
-        }
+        dict_metrics_rrt_frozen, dict_metrics_rrt, \
+        dict_metrics_rrt_udify, \
+        dict_metrics_ronec_frozen, dict_metrics_ronec = init_metrics_dictionaries()
 
-        counter_rrt_frozen = 0
-        counter_rrt = 0
-        counter_rrt_udify = 0
-        counter_ronec_frozen = 0
-        counter_ronec = 0
+        counter_rrt_frozen = counter_rrt = counter_rrt_udify = counter_ronec_frozen = counter_ronec = 0
 
         for filename in os.listdir(os.path.join(args.results_path, lang_model_name)):
             if "rrt_frozen" in filename:
-                with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
-                    dict_metrics = json.load(file)
-
-                for metric in dict_metrics_rrt_frozen:
-                    dict_metrics_rrt_frozen[metric]["precision"] += dict_metrics[metric]["precision"]
-                    dict_metrics_rrt_frozen[metric]["recall"] += dict_metrics[metric]["recall"]
-                    dict_metrics_rrt_frozen[metric]["f1"] += dict_metrics[metric]["f1"]
-
+                update_metrics(filename, lang_model_name, dict_metrics_rrt_frozen)
                 counter_rrt_frozen += 1
-
             elif "rrt_udify" in filename:
-                with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
-                    dict_metrics = json.load(file)
-
-                for metric in dict_metrics_rrt_udify:
-                    dict_metrics_rrt_udify[metric]["precision"] += dict_metrics[metric]["precision"]
-                    dict_metrics_rrt_udify[metric]["recall"] += dict_metrics[metric]["recall"]
-                    dict_metrics_rrt_udify[metric]["f1"] += dict_metrics[metric]["f1"]
-
+                update_metrics(filename, lang_model_name, dict_metrics_rrt_udify)
                 counter_rrt_udify += 1
-
             elif "rrt" in filename:
-                with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
-                    dict_metrics = json.load(file)
-
-                for metric in dict_metrics_rrt:
-                    dict_metrics_rrt[metric]["precision"] += dict_metrics[metric]["precision"]
-                    dict_metrics_rrt[metric]["recall"] += dict_metrics[metric]["recall"]
-                    dict_metrics_rrt[metric]["f1"] += dict_metrics[metric]["f1"]
-
+                update_metrics(filename, lang_model_name, dict_metrics_rrt)
                 counter_rrt += 1
-
             elif "ronec_frozen" in filename:
-                with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
-                    dict_metrics = json.load(file)
-
-                for metric in dict_metrics_ronec_frozen:
-                    dict_metrics_ronec_frozen[metric]["precision"] += dict_metrics[metric]["precision"]
-                    dict_metrics_ronec_frozen[metric]["recall"] += dict_metrics[metric]["recall"]
-                    dict_metrics_ronec_frozen[metric]["f1"] += dict_metrics[metric]["f1"]
-
+                update_metrics(filename, lang_model_name, dict_metrics_ronec_frozen)
                 counter_ronec_frozen += 1
-
             elif "ronec" in filename:
-                with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
-                    dict_metrics = json.load(file)
-
-                for metric in dict_metrics_ronec:
-                    dict_metrics_ronec[metric]["precision"] += dict_metrics[metric]["precision"]
-                    dict_metrics_ronec[metric]["recall"] += dict_metrics[metric]["recall"]
-                    dict_metrics_ronec[metric]["f1"] += dict_metrics[metric]["f1"]
-
+                update_metrics(filename, lang_model_name, dict_metrics_ronec)
                 counter_ronec += 1
 
         print("Language model: {}".format(lang_model_name))
