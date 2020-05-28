0d38326832ca5573ded669b1e062e21aa842161d
avramandrei <avram.andreimarius@gmail.com>
2020-05-28 20:27:21 +0300

    Refactored code in average script.

--- evaluation/tools/results_average.py
+++ evaluation/tools/results_average.py
@@ -57,13 +57,10 @@ def main():
                 with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
                     dict_metrics = json.load(file)
 
-                dict_metrics_rrt_frozen["UPOS"]["precision"] += dict_metrics["UPOS"]["precision"]
-                dict_metrics_rrt_frozen["UPOS"]["recall"] += dict_metrics["UPOS"]["recall"]
-                dict_metrics_rrt_frozen["UPOS"]["f1"] = dict_metrics["UPOS"]["f1"]
-
-                dict_metrics_rrt_frozen["XPOS"]["precision"] += dict_metrics["XPOS"]["precision"]
-                dict_metrics_rrt_frozen["XPOS"]["recall"] += dict_metrics["XPOS"]["recall"]
-                dict_metrics_rrt_frozen["XPOS"]["f1"] += dict_metrics["XPOS"]["f1"]
+                for metric in dict_metrics_rrt_frozen:
+                    dict_metrics_rrt_frozen[metric]["precision"] += dict_metrics[metric]["precision"]
+                    dict_metrics_rrt_frozen[metric]["recall"] += dict_metrics[metric]["recall"]
+                    dict_metrics_rrt_frozen[metric]["f1"] += dict_metrics[metric]["f1"]
 
                 counter_rrt_frozen += 1
 
@@ -71,21 +68,10 @@ def main():
                 with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
                     dict_metrics = json.load(file)
 
-                dict_metrics_rrt_udify["UPOS"]["precision"] += dict_metrics["UPOS"]["precision"]
-                dict_metrics_rrt_udify["UPOS"]["recall"] += dict_metrics["UPOS"]["recall"]
-                dict_metrics_rrt_udify["UPOS"]["f1"] += dict_metrics["UPOS"]["f1"]
-
-                dict_metrics_rrt_udify["UFeats"]["precision"] += dict_metrics["UFeats"]["precision"]
-                dict_metrics_rrt_udify["UFeats"]["recall"] += dict_metrics["UFeats"]["recall"]
-                dict_metrics_rrt_udify["UFeats"]["f1"] += dict_metrics["UFeats"]["f1"]
-
-                dict_metrics_rrt_udify["Lemmas"]["precision"] += dict_metrics["Lemmas"]["precision"]
-                dict_metrics_rrt_udify["Lemmas"]["recall"] += dict_metrics["Lemmas"]["recall"]
-                dict_metrics_rrt_udify["Lemmas"]["f1"] += dict_metrics["Lemmas"]["f1"]
-
-                dict_metrics_rrt_udify["LAS"]["precision"] += dict_metrics["LAS"]["precision"]
-                dict_metrics_rrt_udify["LAS"]["recall"] += dict_metrics["LAS"]["recall"]
-                dict_metrics_rrt_udify["LAS"]["f1"] += dict_metrics["LAS"]["f1"]
+                for metric in dict_metrics_rrt_udify:
+                    dict_metrics_rrt_udify[metric]["precision"] += dict_metrics[metric]["precision"]
+                    dict_metrics_rrt_udify[metric]["recall"] += dict_metrics[metric]["recall"]
+                    dict_metrics_rrt_udify[metric]["f1"] += dict_metrics[metric]["f1"]
 
                 counter_rrt_udify += 1
 
@@ -93,13 +79,10 @@ def main():
                 with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
                     dict_metrics = json.load(file)
 
-                dict_metrics_rrt["UPOS"]["precision"] += dict_metrics["UPOS"]["precision"]
-                dict_metrics_rrt["UPOS"]["recall"] += dict_metrics["UPOS"]["recall"]
-                dict_metrics_rrt["UPOS"]["f1"] += dict_metrics["UPOS"]["f1"]
-
-                dict_metrics_rrt["XPOS"]["precision"] += dict_metrics["XPOS"]["precision"]
-                dict_metrics_rrt["XPOS"]["recall"] += dict_metrics["XPOS"]["recall"]
-                dict_metrics_rrt["XPOS"]["f1"] += dict_metrics["XPOS"]["f1"]
+                for metric in dict_metrics_rrt:
+                    dict_metrics_rrt[metric]["precision"] += dict_metrics[metric]["precision"]
+                    dict_metrics_rrt[metric]["recall"] += dict_metrics[metric]["recall"]
+                    dict_metrics_rrt[metric]["f1"] += dict_metrics[metric]["f1"]
 
                 counter_rrt += 1
 
@@ -107,21 +90,10 @@ def main():
                 with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
                     dict_metrics = json.load(file)
 
-                dict_metrics_ronec_frozen["Entity Type"]["precision"] += dict_metrics["Entity Type"]["precision"]
-                dict_metrics_ronec_frozen["Entity Type"]["recall"] += dict_metrics["Entity Type"]["recall"]
-                dict_metrics_ronec_frozen["Entity Type"]["f1"] += dict_metrics["Entity Type"]["f1"]
-
-                dict_metrics_ronec_frozen["Partial"]["precision"] += dict_metrics["Partial"]["precision"]
-                dict_metrics_ronec_frozen["Partial"]["recall"] += dict_metrics["Partial"]["recall"]
-                dict_metrics_ronec_frozen["Partial"]["f1"] += dict_metrics["Partial"]["f1"]
-
-                dict_metrics_ronec_frozen["Strict"]["precision"] += dict_metrics["Strict"]["precision"]
-                dict_metrics_ronec_frozen["Strict"]["recall"] += dict_metrics["Strict"]["recall"]
-                dict_metrics_ronec_frozen["Strict"]["f1"] += dict_metrics["Strict"]["f1"]
-
-                dict_metrics_ronec_frozen["Exact"]["precision"] += dict_metrics["Exact"]["precision"]
-                dict_metrics_ronec_frozen["Exact"]["recall"] += dict_metrics["Exact"]["recall"]
-                dict_metrics_ronec_frozen["Exact"]["f1"] += dict_metrics["Exact"]["f1"]
+                for metric in dict_metrics_ronec_frozen:
+                    dict_metrics_ronec_frozen[metric]["precision"] += dict_metrics[metric]["precision"]
+                    dict_metrics_ronec_frozen[metric]["recall"] += dict_metrics[metric]["recall"]
+                    dict_metrics_ronec_frozen[metric]["f1"] += dict_metrics[metric]["f1"]
 
                 counter_ronec_frozen += 1
 
@@ -129,25 +101,14 @@ def main():
                 with open(os.path.join(args.results_path, lang_model_name, filename)) as file:
                     dict_metrics = json.load(file)
 
-                dict_metrics_ronec["Entity Type"]["precision"] += dict_metrics["Entity Type"]["precision"]
-                dict_metrics_ronec["Entity Type"]["recall"] += dict_metrics["Entity Type"]["recall"]
-                dict_metrics_ronec["Entity Type"]["f1"] += dict_metrics["Entity Type"]["f1"]
-
-                dict_metrics_ronec["Partial"]["precision"] += dict_metrics["Partial"]["precision"]
-                dict_metrics_ronec["Partial"]["recall"] += dict_metrics["Partial"]["recall"]
-                dict_metrics_ronec["Partial"]["f1"] += dict_metrics["Partial"]["f1"]
-
-                dict_metrics_ronec["Strict"]["precision"] += dict_metrics["Strict"]["precision"]
-                dict_metrics_ronec["Strict"]["recall"] += dict_metrics["Strict"]["recall"]
-                dict_metrics_ronec["Strict"]["f1"] += dict_metrics["Strict"]["f1"]
-
-                dict_metrics_ronec["Exact"]["precision"] += dict_metrics["Exact"]["precision"]
-                dict_metrics_ronec["Exact"]["recall"] += dict_metrics["Exact"]["recall"]
-                dict_metrics_ronec["Exact"]["f1"] += dict_metrics["Exact"]["f1"]
+                for metric in dict_metrics_ronec:
+                    dict_metrics_ronec[metric]["precision"] += dict_metrics[metric]["precision"]
+                    dict_metrics_ronec[metric]["recall"] += dict_metrics[metric]["recall"]
+                    dict_metrics_ronec[metric]["f1"] += dict_metrics[metric]["f1"]
 
                 counter_ronec += 1
 
-        print("Language model: {}\n".format(lang_model_name))
+        print("Language model: {}".format(lang_model_name))
 
         print_task_metrics("RRT Frozen", dict_metrics_rrt_frozen, counter_rrt_frozen)
         print_task_metrics("RRT Non-Frozen", dict_metrics_rrt, counter_rrt)
